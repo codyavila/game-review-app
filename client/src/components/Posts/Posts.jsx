bd84e2d8073c5f78180fd3ef84dceaa912016e74
sanderscody26 <sanderscody26@gmail.com>
2022-08-16 14:25:02 -0400

    Posts and form componenets now show in app

---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from './Post/Posts'
+
+const Posts = () => {
+  return (
+      <>
+        <h1>POSTS</h1>
+        <Post />
+        <Post />
+      </>
+      )
+}
+
+export default Posts
