3843533a7357cc39d0cb97e47e2a57a477a3f1ea
sanderscody26 <sanderscody26@gmail.com>
2022-08-17 15:48:19 -0400

    Update Post Controller Route made

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -36,3 +36,14 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message })
   }
 }
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params
+    const post = req.body
+
+    if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('This ID does not match any posts')
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+
+    res.json(updatedPost)
+}
